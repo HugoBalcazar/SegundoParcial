6b38f94e43bad2f74131d6a756c39ba6d62af573
Hugo <hugo_bp_@hotmail.com>
2019-05-25 12:24:42 -0400

    Commit con ViewModels

---
+++ HugoBalcazarParadaParcial2/app/app/ViewModels/LoginViewModel.cs
@@ -0,0 +1,87 @@
+﻿namespace App.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Windows.Input;
+    using Services;
+    using Xamarin.Forms;
+    using global::App.Models;
+    using Views;
+
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Attributes
+        int nota;
+        bool isrunning;
+        bool isenabled;
+        ApiService apiService;
+        #endregion
+
+        #region Properties
+        public int Nota
+        {
+            get
+            {
+                return this.nota;
+            }
+            set
+            {
+                SetValue(ref this.nota, value);
+            }
+        }
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isrunning;
+            }
+            set
+            {
+                SetValue(ref this.isrunning, value);
+            }
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isenabled;
+            }
+            set
+            {
+                SetValue(ref this.isenabled, value);
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(cmdLogin);
+            }
+        }
+
+        private async void cmdLogin()
+        {
+            IsRunning = true;
+            IsEnabled = false;
+
+            var conexion = await this.apiService.CheckConnection();
+
+            if (!conexion.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                   "ERROR",
+                   conexion.Message,
+                   "Accept");
+                return;
+            }
+
+            #endregion
+
+        }
+    }
+}
